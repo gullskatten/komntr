4388746c8a8976c8319a683d034a57d5e4a43ea5
oyvindhermansen <oyvind@ludensreklame.no>
2019-02-02 21:52:25 +0100

    added hook for api calls

--- src/context/AppTitleContext.js
+++ src/context/AppTitleContext.js
@@ -1,14 +1,14 @@
-import * as React from "react";
+import * as React from 'react';
 
 /**
  * Great post on how to use Context + React hooks:
  * https://dev.to/oieduardorabelo/react-hooks-how-to-create-and-update-contextprovider-1f68
- * 
+ *
  * @Author Eduardo Marcondes Rabelo
- * 
+ *
  */
-const DEFAULT_APP_TITLE = "KOMNTR 😼";
-const DEFAULT_APP_COLOR = "#624694";
+const DEFAULT_APP_TITLE = 'KOMNTR 😼';
+const DEFAULT_APP_COLOR = '#624694';
 let TitleContext = React.createContext();
 
 let initialState = {
@@ -18,12 +18,16 @@ let initialState = {
 
 let reducer = (state, action) => {
   switch (action.type) {
-    case "default-title":
+    case 'default-title':
       return initialState;
-    case "set-title":
-      return {...state, titleColor: action.data.titleColor || "#624694", title: action.data.title };
+    case 'set-title':
+      return {
+        ...state,
+        titleColor: action.data.titleColor || '#624694',
+        title: action.data.title
+      };
     default:
-        return { ...state };  
+      return state;
   }
 };
 
@@ -31,12 +35,19 @@ function TitleContextProvider(props) {
   let [state, dispatch] = React.useReducer(reducer, initialState);
   let value = { state, dispatch };
 
-
   return (
-    <TitleContext.Provider value={value}>{props.children}</TitleContext.Provider>
+    <TitleContext.Provider value={value}>
+      {props.children}
+    </TitleContext.Provider>
   );
 }
 
 let TitleContextConsumer = TitleContext.Consumer;
 
-export { TitleContext, TitleContextProvider, TitleContextConsumer, DEFAULT_APP_COLOR, DEFAULT_APP_TITLE };
+export {
+  TitleContext,
+  TitleContextProvider,
+  TitleContextConsumer,
+  DEFAULT_APP_COLOR,
+  DEFAULT_APP_TITLE
+};
