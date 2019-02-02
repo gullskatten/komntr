a9a5b6ca409fb15a439b6efe4623cc0e418c322f
Espen Gudmundsen <espen@espenschatten.com>
2019-02-02 20:03:20 +0100

    Added more testdata for sample_posts (Coffee&Tea thread).

--- src/data/sample_posts.js
+++ src/data/sample_posts.js
@@ -28,11 +28,47 @@ export default [
     name: 'How to master table tennis',
     description: 'General chat on how to become better in tabletennis.',
     channelId: '3'
-  }.name,
+  },
   {
-    id: '5',
+    id: '6',
     name: 'Brewmasters',
     description: 'General chat on how to become better in the art of coffee brewing.',
+    channelId: '4'
+  },
+  {
+    id: '7',
+    name: 'Coffee date: Oslo',
+    description: 'Best places to go for coffee in Oslo.',
+    channelId: '4'
+  },
+  {
+    id: '8',
+    name: 'Coffee date: Copenhagen',
+    description: 'Got a date? Check out these places in Copenhagen!',
+    channelId: '4'
+  },
+  {
+    id: '9',
+    name: 'Coffee Enthusiast - Stockholm',
+    description: 'We love coffee. We are swedish.',
+    channelId: '4'
+  },
+  {
+    id: '10',
+    name: 'Earl Grey',
+    description: 'General chat for tea enthusiasts.',
+    channelId: '4'
+  },
+  {
+    id: '11',
+    name: 'Starbucks: What\'s best?',
+    description: 'You\'re @ Starbucks, but what should you order?',
+    channelId: '4'
+  },
+  {
+    id: '12',
+    name: 'A Guide to Espresso',
+    description: 'Discussion about brewing the perfect Espresso.',
     channelId: '4'
   }
 ];
