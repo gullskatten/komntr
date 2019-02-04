5ba391af5a912307e1623c84661aeeb6feec4429
Espen G <espen_gud@msn.com>
2019-02-04 01:24:52 +0100

    Users may now add a description to their channel. Channel names have been restricted to 25 chars in length (100 for description).

--- src/components/CreateChannel.js
+++ src/components/CreateChannel.js
@@ -1,17 +1,16 @@
-import React, { useState, useContext } from 'react';
-import styled, { css } from 'styled-components';
-import { fadeInBottom } from '../utils/animations';
-import useApi from '../hooks/useApi';
-import Busy from './Busy';
-import { UserContext } from '../context/UserContext';
-import { TitleContext } from '../context/AppTitleContext';
-import LoginHandler from './LoginHandler';
-import determineColorForString from '../utils/determineColorForString';
-import NoContentFound from './NoResults';
+import React, { useState, useContext } from "react";
+import styled, { css } from "styled-components";
+import { fadeInBottom } from "../utils/animations";
+import useApi from "../hooks/useApi";
+import Busy from "./Busy";
+import { UserContext } from "../context/UserContext";
+import { TitleContext } from "../context/AppTitleContext";
+import LoginHandler from "./LoginHandler";
+import determineColorForString from "../utils/determineColorForString";
+import NoContentFound from "./NoResults";
+import Flex from "../styleguides/Flex";
 
-const MessageFieldWrapper = styled.div`
-  background-color: #222;
-
+const CreateChannelWrapper = styled.div`
   ${props =>
     !props.loggedIn &&
     css`
@@ -29,7 +28,6 @@ const InputWrapper = styled.div`
 `;
 
 const StyledInput = styled.input`
- 
   padding: 0.6rem 0.8rem;
   outline: 0;
   font-size: 2.5rem;
@@ -39,17 +37,47 @@ const StyledInput = styled.input`
   color: #fff;
   outline: none;
   resize: none;
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
   width: 100%;
   height: 135px;
 
   &::placeholder {
     color: #777;
-    font-family: 'Roboto', sans-serif;
+    font-family: "Roboto", sans-serif;
+  }
+
+  @media all and (max-width: 600px) {
+    font-size: 1rem;
+    height: 60px;
   }
 `;
 
-const CreateMessageButton = styled.button`
+const StyledTextArea = styled.textarea`
+  padding: 0.6rem 0.8rem;
+  outline: 0;
+  overflow: hidden;
+  font-size: 1.5rem;
+  border: 0;
+  background-color: #111;
+  color: #fff;
+  outline: none;
+  resize: none;
+  font-family: "Roboto", sans-serif;
+  width: 100%;
+  height: 100px;
+
+  &::placeholder {
+    color: #777;
+    font-family: "Roboto", sans-serif;
+  }
+
+  @media all and (max-width: 600px) {
+    font-size: 1rem;
+    height: 55px;
+  }
+`;
+
+const CreateChannelButton = styled.button`
   outline: 0;
   padding: 0.5rem 1rem;
   background-color: #946ddc;
@@ -64,14 +92,14 @@ const CreateMessageButton = styled.button`
   animation: ${fadeInBottom} 0.25s ease-in-out 0s 1;
 `;
 
-const CreateMessageButtonWrapper = styled.div`
+const CreateChannelButtonWrapper = styled.div`
   display: flex;
   width: 100%;
   height: 40px;
   background-color: #111;
 `;
 
-const CreateMessageButtonText = styled.span`
+const CreateChannelButtonText = styled.span`
   color: #fff;
   font-weight: bold;
   font-size: 1rem;
@@ -85,15 +113,34 @@ const ChannelTag = styled.span`
   margin-right: 10px;
   color: ${props => props.color};
   transition: color 0.5s ease-in-out;
+
+  @media all and (max-width: 600px) {
+    font-size: 3rem;
+  }
 `;
 
-export default function CreateChannel(props) {
+const Error = styled.p`
+  color: #d45f85;
+  margin: 0;
+  padding: 0.5rem 0;
+  font-size: 1.5rem;
+  animation: ${fadeInBottom} 0.25s ease-in-out 0s 1;
 
+  @media all and (max-width: 600px) {
+    font-size: 0.9rem;
+  }
+`;
+
+const CHANNEL_NAME_MAX_LENGTH = 25;
+const CHANNEL_DESCRIPTION_MAX_LENGTH = 100;
+
+export default function CreateChannel(props) {
   const { dispatch } = useContext(TitleContext);
   const userContext = useContext(UserContext);
   const [hasLoadedCategory, setHasLoadedCategory] = useState(false);
-  const [channelName, setChannelName] = useState('');
-  const [channelDescription, setChannelDescription] = useState('');
+  const [channelName, setChannelName] = useState("");
+  const [channelDescription, setChannelDescription] = useState("");
+  const [createChannelError, setCreateChannelError] = useState(undefined);
 
   const {
     match: {
@@ -108,77 +155,116 @@ export default function CreateChannel(props) {
     onSuccess: currentCategory => {
       setHasLoadedCategory(true);
       dispatch({
-        type: 'set-title',
+        type: "set-title",
         data: {
-          title: "Opprett ny channel",
+          title: "Opprett kanal",
           titleColor: "#624694"
         }
       });
     },
     onError: e => {
       setHasLoadedCategory(true);
-    }  
+    }
   });
-
-  function resetInput() {
-    setChannelName('');
-    setChannelDescription('');
-  }
 
   // eslint-disable-next-line
   const [creating, res, err, submitNewChannel] = useApi({
     endpoint: `categories/${categoryId}/channels`,
-    method: 'POST',
+    method: "POST",
     body: {
-     name: channelName, 
-     description: channelDescription
+      name: channelName.trim(),
+      description: channelDescription.trim()
     },
     onSuccess: newChannel => {
       resetInput();
-      props.history.push(`/${categoryId}/${newChannel._id}`)
+      props.history.push(`/${categoryId}/${newChannel._id}`);
     },
     onError: e => {
-      // TODO: Fortell brukeren at dette feilet!
+      setCreateChannelError("Wops! Klarte ikke opprette kanalen.");
     }
   });
 
-  if(hasLoadedCategory && (category === undefined || category === null)) {
-    return (
-      <NoContentFound label="Fant ikke kanalen du lette etter.." />
-    )
+  function resetInput() {
+    setChannelName("");
+    setChannelDescription("");
   }
+
+  function handleInputChange(e, stateUpdaterFunc, maxLength) {
+    const { value } = e.target;
+
+    if (value.trim().length < maxLength) {
+      stateUpdaterFunc(value);
+    }
+  }
+
+  function renderError(errorMessage) {
+    return (
+      <Flex alignItems="center" justify="center">
+        <Error>{errorMessage}</Error>
+      </Flex>
+    );
+  }
+
+  if (hasLoadedCategory && (category === undefined || category === null)) {
+    return <NoContentFound label="Fant ikke kanalen du lette etter.." />;
+  }
+
   return (
     <Busy busy={creating || fetchingCategory}>
-      <MessageFieldWrapper loggedIn={userContext.data.loggedIn}>
+      <CreateChannelWrapper loggedIn={userContext.data.loggedIn}>
         {userContext.data.loggedIn ? (
           <>
             <InputWrapper>
-              <ChannelTag color={determineColorForString(channelName)}>#</ChannelTag>
+              <ChannelTag color={determineColorForString(channelName)}>
+                #
+              </ChannelTag>
               <StyledInput
                 disabled={creating}
                 placeholder="Gi kanalen et navn.."
                 value={channelName}
-                onChange={e => setChannelName(e.target.value)}
+                onChange={e =>
+                  handleInputChange(e, setChannelName, CHANNEL_NAME_MAX_LENGTH)
+                }
               />
             </InputWrapper>
-            <StyledInput
-                disabled={creating}
-                placeholder={`(Valgfritt) Angi en kort beskrivelse av ${channelName || 'kanalen'}..`}
-                value={channelDescription}
-                onChange={e => setChannelDescription(e.target.value)}
-              />
-            <CreateMessageButtonWrapper>
-              {channelName.length > 0 && (
-                <CreateMessageButton onClick={submitNewChannel}>
-                  <CreateMessageButtonText>OPPRETT</CreateMessageButtonText>
-                </CreateMessageButton>
+            {channelName.trim().length === CHANNEL_NAME_MAX_LENGTH - 1 &&
+              renderError(
+                `Navnet kan ikke være lengre enn ${CHANNEL_NAME_MAX_LENGTH} tegn.`
               )}
-            </CreateMessageButtonWrapper>
+            <StyledTextArea
+              disabled={creating}
+              placeholder={`(Valgfritt) Angi en kort beskrivelse av ${channelName.trim() ||
+                "kanalen"}..`}
+              value={channelDescription}
+              onChange={e =>
+                handleInputChange(
+                  e,
+                  setChannelDescription,
+                  CHANNEL_DESCRIPTION_MAX_LENGTH
+                )
+              }
+            />
+            {channelDescription.trim().length ===
+              CHANNEL_DESCRIPTION_MAX_LENGTH - 1 &&
+              renderError(
+                `Beskrivelsen kan ikke være lengre enn ${CHANNEL_DESCRIPTION_MAX_LENGTH} tegn.`
+              )}
+            <CreateChannelButtonWrapper>
+              {channelName.length > 0 && channelName.trim().length > 0 && (
+                <CreateChannelButton onClick={submitNewChannel}>
+                  <CreateChannelButtonText>OPPRETT</CreateChannelButtonText>
+                </CreateChannelButton>
+              )}
+            </CreateChannelButtonWrapper>
+            {createChannelError &&
+              renderError(
+                createChannelError
+              )}
           </>
         ) : (
-          <LoginHandler buttonText="Logg inn med Google for å opprette en channel" />
+          <LoginHandler buttonText="Logg inn med Google for å opprette en kanal" />
         )}
-      </MessageFieldWrapper>
+      </CreateChannelWrapper>
     </Busy>
   );
 }
